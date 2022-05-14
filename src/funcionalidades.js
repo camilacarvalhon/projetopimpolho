a61df26b978b58786e9fb1e85980c5b01464ed11
Camila Carvalho <75643855+camilacarvalhon@users.noreply.github.com>
2022-05-14 17:22:26 -0300

    codificacao tela principal estudante

---
+++ src/funcionalidades.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "100px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementsById("main").style.marginLeft= "0";
+  }
