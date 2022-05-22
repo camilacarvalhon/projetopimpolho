ae36ed28cf93ba5df47e8fe6ae0c943aa9ada792
Camila Carvalho <75643855+camilacarvalhon@users.noreply.github.com>
2022-05-22 17:46:05 -0300

    pagina principal estudante

--- src/funcionalidades.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width = "100px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementsById("main").style.marginLeft= "0";
-  }
