a7f290e0a72970a0079ecb43cd8131fa67eb61f7
Camila Carvalho <75643855+camilacarvalhon@users.noreply.github.com>
2022-07-15 20:21:54 -0300

    ajustes nas telas, funcionalidades

--- public/js/funcionalidades.js
+++ public/js/funcionalidades.js
@@ -17,15 +17,35 @@ function recuperarProfessor() {
 
 
 /*------------------------------------------------------------------------------------------------------- */
-
+/** 
+ * Função que verifica se o campo está vazio
+ * @param {*} valor 
+ * @returns boolean 
+ */
+function isEmpty(valor) {
+    if (valor && valor != '' && valor != null && valor != undefined) {
+        return false;
+    }
+    return true;
+}
 //Função Cadastrar Estudante 
-
 function enviarEstudante() {
-    let nome = $('#nome').val()
-    let email = $('#email').val()
-    let senha = $('#senha').val()
-    let anoturma = $('#anoturma').val()
+    let nome = $('#nome').val();
+    let email = $('#email').val();
+    let senha = $('#senha').val();
+    let anoturma = $('#anoturma').val();
+    validarCampos(email, nome, senha, anoturma, true);
+}
 
+
+/**
+ * Função que envia os dados para ser salvos no banco
+ * @param {string} nome 
+ * @param {string} email 
+ * @param {string} senha 
+ * @param {string} anoturma 
+ */
+function postEstudante(nome, email, senha, anoturma) {
     $.ajax(
         {
             type: 'POST',
@@ -38,22 +58,72 @@ function enviarEstudante() {
             }),
             contentType: 'application/json',
             success: function (resposta) {
-                window.location.href = "pag_login_estudante.html"
+                window.location.href = "pag_login_estudante.html";
             }
         }
     );
 }
+/**
+ * Função que valida os dados do cadastro do uusuário
+ * @param {string} email 
+ * @param {string} nome 
+ * @param {string} senha 
+ * @param {string} anoturma 
+ * @param {boolean} ehEstudante 
+ */
+function validarCampos(email, nome, senha, anoturma, ehEstudante) {
+    let msgErro =  document.querySelector('#msgAlertaErroCad')
+    const emailReg = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+    const passwordRegx = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{8,})");
+    // Verificando os campos
+    if (isEmpty(nome)) {
+        msgErro.innerHTML= "Erro: Necessário preencher o campo nome!";
+    } else if (isEmpty(email)) {
+        msgErro.innerHTML= "Erro: Necessário preencher o campo email!";
+    } else if (isEmpty(senha)) {
+        msgErro.innerHTML= "Erro: Necessário preencher o campo senha!";
+    } else if (isEmpty(anoturma)) {
+        msgErro.innerHTML= "Erro: Necessário preencher o campo turma!";
+    } else {
+        // Verificando se o email é válido
+        if (!emailReg.test(email)) {
+            msgErro.innerHTML= "Erro: Formato inválido de email! Insira um @ email";
+        } else if (!passwordRegx.test(senha)) {
+            msgErro.innerHTML= "Erro: A senha deve conter entre 8 a 15 caracteres,<br> deve conter pelo menos uma letra maiúscula<br> um número ";
+        } else {
+            // Verifica se é estudante ou professor, para saber onde tera que salvar os dados
+            if (ehEstudante == true) {
+                postEstudante(nome, email, senha, anoturma);
+            } else {
+                postProfessor(nome, email, senha, anoturma);
+            }
 
+        }
+
+    }
+}
 
 /*------------------------------------------------------------------------------------------------------- */
 
 //Função Cadastrar Professor
 
 function enviarProfessor() {
-    let nome = $('#nome').val()
-    let email = $('#email').val()
-    let senha = $('#senha').val()
-    let anoturma = $('#anoturma').val()
+    let nome = $('#nome').val();
+    let email = $('#email').val();
+    let senha = $('#senha').val();
+    let anoturma = $('#anoturma').val();
+    validarCampos(email, nome, senha, anoturma, false);
+
+}
+
+/**
+ * Função que envia os dados para ser salvos no banco
+ * @param {string} nome 
+ * @param {string} email 
+ * @param {string} senha 
+ * @param {string} anoturma 
+ */
+function postProfessor(nome, email, senha, anoturma) {
     $.ajax(
         {
             type: 'POST',
@@ -76,30 +146,30 @@ function enviarProfessor() {
 
 //Função Acessar - realizar consulta Estudante
 function acessar() {
-    let email = $('#email').val()
-    let senha = $('#senha').val()
+    let email = $('#emailestudante').val();
+    let senha = $('#senhaestudante').val();
 
     // validação 
-    if(email){
+    if (!isEmpty(email) && !isEmpty(senha)) {
         $.ajax(
             {
                 type: 'GET',
                 url: `${window.location.origin}/aluno/${email}/${senha}`,
                 success: function (resposta) {
-                    localStorage.removeItem('nome_estudante');
+                    // localStorage.removeItem('nome_estudante');
                     localStorage.setItem('nome_estudante', resposta.nome);
                     localStorage.setItem('nome_jogo', JSON.stringify(resposta.nomejogo));
-                    localStorage.setItem('pontos_jogo', resposta.pontosjogo)
-                    window.location.href = "pag_acessar_estudante.html"
-                   
+                    localStorage.setItem('pontos_jogo', resposta.pontosjogo);
+                    window.location.href = "pag_acessar_estudante.html";
+
                 },
                 error: function (resposta) {
                     alert(`Usuário não está cadastrado!`);
                 }
             }
         );
-    }else{
-        alert('Email ou senha inválida! ')
+    } else {
+        alert('Email ou senha inválida! ');
     }
 }
 /*------------------------------------------------------------------------------------------------------- */
@@ -107,24 +177,29 @@ function acessar() {
 //Função Acessar - realizar consulta Professor
 
 function acessarProfessor() {
-    let email = $('#emailprof').val()
-    let senha = $('#senhaprof').val()
+    let email = $('#emailprof').val();
+    let senha = $('#senhaprof').val();
 
     // validação 
-    $.ajax(
-        {
-            type: 'GET',
-            url: `${window.location.origin}/professor/${email}/${senha}`,
-            success: function (resposta) {
+    if (!isEmpty(email) && !isEmpty(senha)) {
+        $.ajax(
+            {
+                type: 'GET',
+                url: `${window.location.origin}/professor/${email}/${senha}`,
+                success: function (resposta) {
 
-                alert(`Usuário ${resposta.nomeprof} está cadastrado!`);
-                window.location.href = "pag_acessar_professor.html"
-            },
-            error: function (resposta) {
-                alert(`Usuário não está cadastrado!`);
+                    alert(`Usuário ${resposta.nomeprof} está cadastrado!`);
+                    window.location.href = "pag_acessar_professor.html";
+                },
+                error: function (resposta) {
+                    alert(`Usuário não está cadastrado!`);
+                }
             }
-        }
-    );
+        );
+    } else {
+        alert('Email ou senha inválida! ');
+    }
+
 }
 
 
@@ -154,7 +229,7 @@ function atualizar_senha() {
             contentType: 'application/json',
             success: function (resposta) {
                 alert(`Dados atualizados com sucesso!`);
-                window.location.href = "pag_login_estudante.html"
+                window.location.href = "pag_login_estudante.html";
             }
         }
     );
@@ -186,7 +261,7 @@ function atualizar_senha_professor() {
             contentType: 'application/json',
             success: function (resposta) {
                 alert(`Dados atualizados com sucesso!`);
-                window.location.href = "pag_login_professor.html"
+                window.location.href = "pag_login_professor.html";
             }
         }
     );
@@ -222,5 +297,5 @@ function consultarJogo() {
 // Fazer o post dos pontos dos jogos
 
 function atualizar() {
-    windows.onload()
+    windows.onload();
 }
